4d59a38dabe27b411ce82f8e5a3049e6cc2e8df8
harry <h2choi@ucsd.edu>
2023-09-15 15:39:06 -0700

    moving computer

--- subcompare_nextjs/src/app/layout.js
+++ subcompare_nextjs/src/app/layout.js
@@ -1,8 +1,5 @@
 import './globals.css';
-// import { Inter } from 'next/font/google';
 import Head from 'next/head';
-
-// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'SubCompare',
@@ -15,12 +12,10 @@ export default function RootLayout({ children }) {
       <Head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
-        {/* connect to google font */}
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
       </Head>
-      {/* <body className={inter.className}>{children}</body> */}
       <body>{children}</body>
     </html>
   )
