5fa07ddaf3f177efd3f59581eb28ec15c84f8f6b
Hyunwoo36 <h2choi@ucsd.edu>
2023-10-01 01:45:38 -0700

    mobile adjusting done

--- subcompare_nextjs/src/app/components/serviceCategoryItems.js
+++ subcompare_nextjs/src/app/components/serviceCategoryItems.js
@@ -13,63 +13,63 @@ export default function ServiceCategoryItems() {
             <div className={`${styles.flexAlignItemsCenter} ${styles.fontLarger}
             ${currentCategory === "OTT / Streaming" ? styles.bold : ""}`}
                 onClick={() => setCurrentCategory("OTT / Streaming")}>
-                <TriangleIcon className={styles.marginRight} />
+                <TriangleIcon className={`${styles.marginRight} ${styles.triangleIconSize}`} />
                 OTT / Streaming
             </div>
 
             <div className={`${styles.flexAlignItemsCenter} ${styles.fontLarger}
             ${currentCategory === "Music" ? styles.bold : ""}`}
                 onClick={() => setCurrentCategory("Music")}>
-                <TriangleIcon className={styles.marginRight} />
+                <TriangleIcon className={`${styles.marginRight} ${styles.triangleIconSize}`} />
                 Music
             </div>
 
             <div className={`${styles.flexAlignItemsCenter} ${styles.fontLarger}
             ${currentCategory === "Fitness" ? styles.bold : ""}`}
                 onClick={() => setCurrentCategory("Fitness")}>
-                <TriangleIcon className={styles.marginRight} />
+                <TriangleIcon className={`${styles.marginRight} ${styles.triangleIconSize}`} />
                 Fitness
             </div>
 
             <div className={`${styles.flexAlignItemsCenter} ${styles.fontLarger}
             ${currentCategory === "E-Book and Audiobook" ? styles.bold : ""}`}
                 onClick={() => setCurrentCategory("E-Book and Audiobook")}>
-                <TriangleIcon className={styles.marginRight} />
+                <TriangleIcon className={`${styles.marginRight} ${styles.triangleIconSize}`} />
                 E-Book and Audiobook
             </div>
 
             <div className={`${styles.flexAlignItemsCenter} ${styles.fontLarger}
             ${currentCategory === "Learning / Course Platforms" ? styles.bold : ""}`}
                 onClick={() => setCurrentCategory("Learning / Course Platforms")}>
-                <TriangleIcon className={styles.marginRight} />
+                <TriangleIcon className={`${styles.marginRight} ${styles.triangleIconSize}`} />
                 Learning / Course Platforms
             </div>
 
             <div className={`${styles.flexAlignItemsCenter} ${styles.fontLarger}
             ${currentCategory === "Magazine / News Subscription" ? styles.bold : ""}`}
                 onClick={() => setCurrentCategory("Magazine / News Subscription")}>
-                <TriangleIcon className={styles.marginRight} />
+                <TriangleIcon className={`${styles.marginRight} ${styles.triangleIconSize}`} />
                 Magazine / News Subscription
             </div>
 
             <div className={`${styles.flexAlignItemsCenter} ${styles.fontLarger}
             ${currentCategory === "Meal Delivery / Kit" ? styles.bold : ""}`}
                 onClick={() => setCurrentCategory("Meal Delivery / Kit")}>
-                <TriangleIcon className={styles.marginRight} />
+                <TriangleIcon className={`${styles.marginRight} ${styles.triangleIconSize}`} />
                 Meal Delivery / Kit
             </div>
 
             <div className={`${styles.flexAlignItemsCenter} ${styles.fontLarger}
             ${currentCategory === "Food Delivery" ? styles.bold : ""}`}
                 onClick={() => setCurrentCategory("Food Delivery")}>
-                <TriangleIcon className={styles.marginRight} />
+                <TriangleIcon className={`${styles.marginRight} ${styles.triangleIconSize}`} />
                 Food Delivery
             </div>
 
             <div className={`${styles.flexAlignItemsCenter} ${styles.fontLarger}
             ${currentCategory === "Dating App" ? styles.bold : ""}`}
                 onClick={() => setCurrentCategory("Dating App")}>
-                <TriangleIcon className={styles.marginRight} />
+                <TriangleIcon className={`${styles.marginRight} ${styles.triangleIconSize}`} />
                 Dating App
             </div>
 
--- subcompare_nextjs/src/app/layout.js
+++ subcompare_nextjs/src/app/layout.js
@@ -16,6 +16,7 @@ export default function RootLayout({ children }) {
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
         <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body>{children}</body>
     </html>
